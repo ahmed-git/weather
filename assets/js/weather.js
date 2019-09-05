fb3934817e1d3b2aa3072a951f7c1c5965d7da91
ahmed-git <53937818+ahmed-git@users.noreply.github.com>
2019-09-05 11:26:29 +0100

    add API key

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,5 +1,5 @@
 
-const appKey = "key";// key for weather bit API
+const appKey = "08395cda90b14fc7ba471ce0020c4707";// key for weather bit API
 const weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Juin", "July", "Aug", "Sep", "Oct", "Nov", "Dec"]
 let sendButton = document.getElementById("sendButton");
